a9e72f1c79d4861968ebef07189f0fdf0ed697ab
wcs0228 <chse0226>
2019-11-05 12:32:04 +0800

    project

--- src/test.java
+++ src/test.java
@@ -377,7 +377,7 @@ public class test extends JFrame implements ActionListener,ItemListener {
             dlg2.setVisible(true);
             no3.setVisible(false);
         }
-        else if(number =="1"||number =="2"||number =="3"||number =="4"||number =="5"||number =="6"||number =="7"||number =="8"||number =="9"||number =="10"||number =="11"||number =="12"||number =="13"||number =="14")
+        else if(number =="1"||number =="2"||number =="3"||number =="4"||number =="5"||number =="6"||number =="7"||number =="8"||number =="9"||number =="10"||number =="11"||number =="12"||number =="13"||number =="14"||number =="16")
         {
             no3.setVisible(true);
             chc.select(""); //很重要，讓他每一次都重置，才不會讓錯誤的物件一直卡著
